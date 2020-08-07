a10ba35027c3e689bf062b3325d1527ce61f54dd
Juan Carlos Alonso Valenzuela <juaaloval@gmail.com>
2020-08-07 04:13:06 +0200

    Funcionalidad finalizada

--- src/frontend/index/controller.js
+++ src/frontend/index/controller.js
@@ -17,5 +17,100 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
 
-
+// $scope.peliculas = $.getJSON("./model.json");
 $scope.date = new Date().toISOString();
+$scope.cesta = {};
+$scope.total = 0;
+$scope.presupuesto = 100;
+
+const generosDisponibles = ["Acción", "Aventura", "Drama", "Cyberpunk", "Ciencia Ficción", "Western"]
+
+$scope.addMovie = function(pelicula){
+    $scope.total = 0;
+    if(!$scope.cesta[pelicula.titulo]){
+        $scope.cesta[pelicula.titulo] = pelicula;
+        $scope.cesta[pelicula.titulo].cantidad = 0;
+    }
+    $scope.cesta[pelicula.titulo].cantidad += 1;
+    this.model.peliculas[pelicula.titulo].unidades--;
+    calcularTotal();
+}
+
+$scope.eliminarPelicula = function(pelicula) {
+    delete this.model.peliculas[pelicula.titulo];
+}
+
+$scope.vaciarCesta = function(){
+    let model2 = this.model;
+    Object.keys($scope.cesta).forEach(function(key){
+        // Añadir el stock
+        // this.model.peliculas[key].unidades += $scope.cesta[key].cantidad;
+        model2.peliculas[key].unidades += $scope.cesta[key].cantidad;
+    })
+    $scope.cesta = {};
+    $scope.total = 0;
+}
+
+$scope.borrarDeCesta = function(compra){
+    this.model.peliculas[compra.titulo].unidades += $scope.cesta[compra.titulo].cantidad
+    delete $scope.cesta[compra.titulo]; 
+    calcularTotal();
+}
+
+$scope.pagar = function(){
+    if($scope.presupuesto >= $scope.total){
+        $scope.presupuesto = $scope.presupuesto - $scope.total;
+        $scope.cesta = {};
+        $scope.total = 0;
+    }else{
+        alert("El presupuesto es insuficiente para realizar el pago");
+    }
+}
+
+$scope.crearPelicula = function(){
+    
+    if(this.Generos == null){
+        alert("Por favor, introduzca al menos un género");
+    }else if($scope.FechaEstreno > new Date()){
+        alert("La fecha de estreno debe ser anterior a la actual");
+    }else{
+
+        let listaGeneros = [];
+        let auxList = this.Generos;
+        Object.keys(this.Generos).forEach(function(key){
+            if(auxList[key]){
+                listaGeneros.push(generosDisponibles[key]);
+            }
+        })
+
+        let nuevaPelicula = {
+            "titulo": $scope.Titulo,
+            "sinopsis": $scope.Sinopsis,
+            "fechaEstreno": $scope.FechaEstreno,
+            "precio": $scope.Precio,
+            "unidades": $scope.Unidades,
+            "imagen": $scope.Imagen,
+            "generos": listaGeneros
+        }
+        this.model.peliculas[$scope.Titulo] = nuevaPelicula;
+    }
+    
+}
+
+
+$scope.incrementar = function(pelicula){
+    this.model.peliculas[pelicula.titulo].unidades++;
+}
+
+$scope.decrementar = function(pelicula){
+    this.model.peliculas[pelicula.titulo].unidades--;
+}
+
+function calcularTotal(){
+    $scope.total = 0;
+    Object.keys($scope.cesta).forEach(function(key){
+        var pelicula = $scope.cesta[key]
+        $scope.total = $scope.total + (pelicula.precio * pelicula.cantidad)
+    })
+}
+
